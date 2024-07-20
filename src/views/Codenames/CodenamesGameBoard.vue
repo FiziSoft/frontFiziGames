802833ea88205719349b2fcf6daf6118781f82fa
Alex Arkhipenko <fizi.ua@gmail.com>
2024-07-21 01:02:51 +0300

    sad

--- src/views/Codenames/CodenamesGameBoard.vue
+++ src/views/Codenames/CodenamesGameBoard.vue
@@ -93,7 +93,6 @@ const router = useRouter();
 const gameId = ref(route.params.gameId);
 
 if (route.params.locale) {
-  alert(locale.value)
   locale.value = route.params.locale;
 }
 
@@ -174,6 +173,9 @@ const connectWebSocket = () => {
         bluePlayersCount.value = message.bluePlayersCount;
 
         wordsLoaded.value = true;  
+        
+        // Set locale from server
+        locale.value = message.locale;
       } else if (message.type === "startGame") {
         gameStarted.value = true;
         showColors.value = true;
@@ -293,8 +295,7 @@ onMounted(() => {
   background: rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: center;
-  align-items: center;
-  z-index: 1000;
+  align-items: center
 }
 
 .modal-content-unique {
@@ -302,27 +303,5 @@ onMounted(() => {
   padding: 20px;
   border-radius: 8px;
   text-align: center;
-}
-
-/* Анимация загрузки */
-.loading-spinner {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-}
-
-.spinner {
-  border: 8px solid rgba(0, 0, 0, 0.1);
-  border-top: 8px solid #3498db;
-  border-radius: 50%;
-  width: 60px;
-  height: 60px;
-  animation: spin 1s linear infinite;
-}
-
-@keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
 }
 </style>
