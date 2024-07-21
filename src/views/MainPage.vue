aad6c8c246acceb1f93c9be295c73ac0b1db0046
Alex Arkhipenko <fizi.ua@gmail.com>
2024-07-21 13:51:06 +0300

    da

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -34,8 +34,8 @@ import { ref, onMounted } from 'vue';
 import { useI18n } from 'vue-i18n';
 import ModalMain from '@/components/ModalMain.vue';
 
-// const network_url = 'https://fizigames-799b6804c93a.herokuapp.com';
-const network_url = 'http://localhost:8080';
+const network_url = 'https://fizigames-799b6804c93a.herokuapp.com';
+// const network_url = 'http://localhost:8080';
 
 const games = ref([
   {
