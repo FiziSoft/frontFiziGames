3cde47d76a3f8324c8bf7ae5c3fb93a74863e362
Alex Arkhipenko <fizi.ua@gmail.com>
2024-08-22 23:55:02 +0300

    dsdsa

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,33 +1,55 @@
+const CACHE_NAME = 'fizigames-cache-v1'; // Обновите версию кеша
+
+// Файлы, которые необходимо закешировать
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js',
+  '/manifest.json',
+  '/src/assets/style.sass',
+  '/src/assets/sound/60sec.mp3',
+  '/src/assets/sound/plus_click.mp3',
+  '/src/assets/sound/miss_sound.mp3',
+  '/src/assets/whatsapp.png',
+  '/src/assets/viber.png',
+  '/src/assets/telegram.png',
+
+  // добавьте другие важные файлы
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-      caches.open('fizigames-cache-v1').then(cache => {
-          return cache.addAll([
-              '/',
-              '/index.html',
-              '/css/style.css',
-              '/js/main.js',
-              '/manifest.json',
-              '/src/assets/style.sass',
-          
-              '/src/views/Alias/AliasCreate.vue',
-              '/src/views/Alias/AliasRoom.vue',
-              '/src/assets/sound/60sec.mp3',
-              '/src/assets/sound/plus_click.mp3',
-              '/src/assets/sound/miss_sound.mp3',
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
 
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME]; // Оставляем только текущий кеш
 
-              // Добавьте все другие необходимые файлы сюда
-          ]);
-      })
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            // Удаляем старые кеши
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-      caches.match(event.request).then(response => {
-          return response || fetch(event.request).catch(() => {
-              return caches.match('/offline.html');
-          });
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        // Если запрашиваемый ресурс не найден в кеше и недоступен из сети, возвращаем закешированную офлайн-страницу
+        return caches.match('/index.html');
+      });
+    })
   );
 });
